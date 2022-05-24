3522483f11d468e438d78c5a00a775ebed2b804f
Michael Ilyin <milyin@gmail.com>
2022-05-25 00:46:14 +0300

    handle attribute weak_name

--- src/lib.rs
+++ src/lib.rs
@@ -7,20 +7,45 @@
 //!
 
 use proc_macro::TokenStream;
-// use syn::{parse_macro_input, DeriveInput};
 use quote::quote;
+use syn::{parse::Parse, parse_macro_input, Attribute, DeriveInput, Ident};
+
+fn is_attr_named_fn<'a>(name: &'a str) -> impl Fn(&Attribute) -> bool + 'a {
+    move |attr| {
+        if let Some(attr_ident) = attr.path.get_ident() {
+            if attr_ident == name {
+                return true;
+            }
+        }
+        false
+    }
+}
+
+fn take_attr<T: Parse>(name: &str, attrs: &mut Vec<Attribute>) -> Option<T> {
+    if let Some(pos) = attrs.iter().position(is_attr_named_fn(name)) {
+        let attr = attrs.remove(pos);
+        Some(attr.parse_args::<T>().unwrap())
+    } else {
+        None
+    }
+}
 
 #[proc_macro_derive(Weak, attributes(weak_name, weak))]
-pub fn derive_weak(_input: TokenStream) -> TokenStream {
-    // let input: DeriveInput = parse_macro_input!(input);
+pub fn derive_weak(input: TokenStream) -> TokenStream {
+    let mut input: DeriveInput = parse_macro_input!(input);
+    let weak_ident = take_attr("weak_name", &mut input.attrs).unwrap_or(Ident::new(
+        format!("W{}", input.ident).as_str(),
+        input.ident.span(),
+    ));
+
     quote! {
-        struct WFoo {
+        struct #weak_ident {
             foo: usize,
             bar: std::rc::Weak<usize>
         }
 
         impl Foo {
-            pub fn downgrade(&self) -> WFoo {
+            pub fn downgrade(&self) -> #weak_ident {
                 WFoo {
                     foo: self.foo.clone(),
                     bar: std::rc::Rc::downgrade(&self.bar)
@@ -28,7 +53,7 @@ pub fn derive_weak(_input: TokenStream) -> TokenStream {
             }
         }
 
-        impl WFoo {
+        impl #weak_ident {
             pub fn upgrade(&self) -> Option<Foo> {
                 let bar = if let Some(v) = self.bar.upgrade() {
                     v
--- tests/test.rs
+++ tests/test.rs
@@ -1,6 +1,7 @@
 use derive_weak::Weak;
 
 #[derive(Weak)]
+#[weak_name(WeakFoo)]
 struct Foo {
     foo: usize,
     bar: std::rc::Rc<usize>,
